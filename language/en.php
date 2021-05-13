97745f82623f68bd1476db330aeee29c24414132
smekac <stojkic95@hotmail.com>
2021-05-13 13:50:05 +0200

    added two language with php aproach

---
+++ language/en.php
@@ -0,0 +1,25 @@
+<?php
+$lang = array(
+    "main_txt"=>"We offer the possibility of investing in a deposit or buying pieces",
+    "home_txt"=>"Translate page",
+    "en_lang" => "English",
+    "sr_lang" => "Serbian",
+    "about"   => "About",
+    "services" => "Services",
+    "portfolio" => "Portfolio",
+    "contact" => "Contact",
+    "find_Out_More" => "Find Out More",
+    "this_metal" => "The given ore sample has several different precious metals",
+    "we_have" => "We've got what you need!",
+    "get_Started" => "Get Started!",
+    "category" => "Category",
+    "in_Touch" => "Let's Get In Touch!",
+    "ready_to_start" => "Ready to start your next project with us? Give us a call or send us an email and we will get back to you as soon as possible!",
+    "company_name" => "- Company Name"
+    
+    
+
+    
+);
+
+?>
---
+++ language/sr.php
@@ -0,0 +1,21 @@
+<?php
+$lang = array(
+    "main_txt" => "Nudimo mogućnost ulaganja u ležište ili otkupljivanje komada",
+    "home_txt"=>"Translate page",
+    "en_lang" => "Engleski",
+    "sr_lang" => "Sprski",
+    "about"   => "O nama",
+    "services" => "Usluge",
+    "portfolio" => "Primerci",
+    "contact" => "Kontakti",
+    "find_Out_More" => "Saznajte više",
+    "this_metal" => "Dati uzorak rude poseduje više različitih plemenitih metala pronadjeni su uglavnom na području balkana",
+    "we_have" => "Imamo ono što vam treba",
+    "get_Started" => "Počnimo",
+    "category" => "Procentualni udeo u rudi ispod slike",
+    "in_Touch" => "Stupimo u kontakt",
+    "ready_to_start" => "Jeste li spremni započeti svoj sljedeći projekt s nama? Pozovite nas ili nam pošaljite e-mail i javićemo vam se u najkraćem mogućem roku!",
+    "company_name" => "- Company Name"
+);
+
+?>
